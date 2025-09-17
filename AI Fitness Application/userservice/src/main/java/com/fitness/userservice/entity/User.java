12f9da31f0ea4eb833b7d8c93cc48abe83064d56
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-09-17 14:10:20 +0530

    Initial commit - Spring Boot projects and files

---
+++ AI Fitness Application/userservice/src/main/java/com/fitness/userservice/entity/User.java
@@ -0,0 +1,36 @@
+package com.fitness.userservice.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID )
+    private String id;
+
+    @Column(nullable = false,  unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+    
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
